3780f2a868c1eedab2b06bd52b60e1f5ea2426fa
R Y OK <33168025+usshen@users.noreply.github.com>
2020-11-21 19:42:37 -0800

    Add files via upload

---
+++ monoTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using MonoTorrent;
+
+namespace PSmono
+{
+    public class PSMonoOBJ
+    {
+        public PSMonoOBJ()
+        {
+            
+        }
+
+        public string Hashinfomation(string torrentfile)
+        {
+            InfoHash Tinfohash;
+            Tinfohash = MonoTorrent.Torrent.Load(torrentfile).InfoHash;
+            return Tinfohash.ToHex();
+        }
+    }
+}
